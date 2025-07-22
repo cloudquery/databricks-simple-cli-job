2dad583a9041f90a36f596800b2c4ae711dc7d90
Mariano Gappa <spinetta@gmail.com>
2025-07-22 11:04:39 -0300

    Expand all secrets automatically.

--- main.py
+++ main.py
@@ -3,28 +3,18 @@
 Script to download cloudquery binary, set up environment, and run sync command.
 """
 
+import argparse
 import os
 import sys
 import subprocess
 from pyspark.dbutils import DBUtils
 
-def load_environment():
+def load_environment(scope):
     """Load environment variables from .env file if it exists."""
     dbutils = DBUtils(spark)
-
-    os.environ["AWS_ACCESS_KEY_ID"] = dbutils.secrets.get(scope="mariano", key="AWS_ACCESS_KEY_ID")
-    os.environ["AWS_SECRET_ACCESS_KEY"] = dbutils.secrets.get(scope="mariano", key="AWS_SECRET_ACCESS_KEY")
-    os.environ["AWS_SESSION_TOKEN"] = dbutils.secrets.get(scope="mariano", key="AWS_SESSION_TOKEN")
-
-    os.environ["CLOUDQUERY_API_KEY"] = dbutils.secrets.get(scope="mariano", key="CLOUDQUERY_API_KEY")
-
-    os.environ["DATABRICKS_CATALOG"] = dbutils.secrets.get(scope="mariano", key="DATABRICKS_CATALOG")
-    os.environ["DATABRICKS_SCHEMA"] = dbutils.secrets.get(scope="mariano", key="DATABRICKS_SCHEMA")
-    os.environ["DATABRICKS_STAGING_PATH"] = dbutils.secrets.get(scope="mariano", key="DATABRICKS_STAGING_PATH")
-    os.environ["DATABRICKS_HTTP_PATH"] = dbutils.secrets.get(scope="mariano", key="DATABRICKS_HTTP_PATH")
-    os.environ["DATABRICKS_HOSTNAME"] = dbutils.secrets.get(scope="mariano", key="DATABRICKS_HOSTNAME")
-    os.environ["DATABRICKS_ACCESS_TOKEN"] = dbutils.secrets.get(scope="mariano", key="DATABRICKS_ACCESS_TOKEN")
-
+    metas = dbutils.secrets.list(scope=scope)
+    for meta in metas:
+        os.environ[meta.key] = dbutils.secrets.get(scope=scope, key=meta.key)
 
 def expand_env_vars_in_yaml(yaml_file):
     """Expand environment variables in the YAML file."""
@@ -64,12 +54,13 @@ def main():
     """Main function to orchestrate the entire process."""
     # Parse command line arguments
     parser = argparse.ArgumentParser(description='Run cloudquery sync with specified YAML file')
-    parser.add_argument('yaml_file', help='Path to the YAML configuration file')
+    parser.add_argument('spec', help='Path to the YAML configuration file')
+    parser.add_argument('scope', help='Scope to load Databricks secrets from')
     args = parser.parse_args()
 
-    load_environment()
+    load_environment(args.scope)
     
-    yaml_file = args.yaml_file
+    yaml_file = args.spec
     expanded_yaml = expand_env_vars_in_yaml(yaml_file)
     
     run_cloudquery_sync(expanded_yaml)
